1c754e1167dc1bd942960a069346a1e0f8f9b902
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-10-29 21:06:15 +0200

    Lua mode, binaries, build scripts, docs

--- dasm_x64.lua
+++ dasm_x64.lua
@@ -8,5 +8,14 @@
 -- All the interesting stuff is there.
 ------------------------------------------------------------------------------
 
-x64 = true -- Using a global is an ugly, but effective solution.
-return require("dasm_x86")
+--unload dasm_x86 if it's already loaded.
+local dasm_x86 = package.loaded.dasm_x86
+package.loaded.dasm_x86 = nil
+
+rawset(_G, 'x64', true) -- Using a global is an ugly, but effective solution.
+local dasm_x64 = require("dasm_x86")
+
+package.loaded.dasm_x86 = dasm_x86 --put it back
+
+return dasm_x64
+
